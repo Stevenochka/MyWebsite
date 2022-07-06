ddd7ba48475427d678551e17f10b2a393d71a3c7
Stevenochka <redlemmon714@gmail.com>
2022-07-06 08:09:32 +0300

    yeahh

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+  $(".slider-inner").slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    infinite: false,
+  });
+});
